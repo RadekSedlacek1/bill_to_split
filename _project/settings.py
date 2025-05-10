27124c9dbde42d4ef847c04a5493dd72339d922f
RadekSedlacek1 <sedlacek.radek@seznam.cz>
2025-05-10 12:13:51 +0200

    Oprava statickych souboru

--- _project/settings.py
+++ _project/settings.py
@@ -118,11 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 STATICFILES_DIRS = [
-	BASE_DIR / 'static'
-	]
+    BASE_DIR / 'static',                        # Project static files
+    BASE_DIR / 'main' / 'static' / 'main',      # App static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
